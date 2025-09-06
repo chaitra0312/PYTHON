7c9e69c9f1bbc43d51f3b7a3df745cf5ddb7d633
CHAITRA <164479161+chaitra0312@users.noreply.github.com>
2025-09-06 18:56:37 +0530

    Create triangle.py

---
+++ DSA/Patterns/triangle.py
@@ -0,0 +1,10 @@
+n=int(input('enter'))
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end ="")
+    for k in range(2*i - 1):
+        if(k==0 or k==2*i-2 or i==n):
+             print('*',end='')
+        else:
+            print(" ",end="")
+    print()
